a20905dcebf66d9fcf3caad23d70db84fab5f878
Zina Assi <zina.assi@campus.technion.ac.il>
2025-04-11 22:05:29 +0300

    Add API key for Google Maps and location usage descriptions in iOS and Android manifests

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBIHDIjMDitDD-JFHllxBQNnUKOxm5Mz50")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
